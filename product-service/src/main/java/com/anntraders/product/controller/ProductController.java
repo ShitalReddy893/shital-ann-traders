647fd9421aaa239978e20e29fcac00e2882f63d2
ShitalReddy893 <mahadevr897@gmail.com>
2025-09-19 13:28:52 +0530

    Update ProductController.java
    updated Put and post method

--- product-service/src/main/java/com/anntraders/product/controller/ProductController.java
+++ product-service/src/main/java/com/anntraders/product/controller/ProductController.java
@@ -1,19 +1,30 @@
 package com.anntraders.product.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.anntraders.product.model.Product;
 import com.anntraders.product.service.ProductService;
 
-import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
-@CrossOrigin(origins = "*") // Angular
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+
 public class ProductController {
 
-    @Autowired
+   @Autowired
     private final ProductService service;
 
     public ProductController(ProductService service) {
@@ -22,7 +33,8 @@ public class ProductController {
 
     @GetMapping
     public List<Product> getAllProducts() {
-        return service.getAllProducts();
+    	List<Product> list=service.getAllProducts();
+        return list ;
     }
 
     @GetMapping("/{id}")
@@ -30,7 +42,7 @@ public class ProductController {
         return service.getProductById(id);
     }
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Product addProduct(@RequestBody Product product) {
         return service.saveProduct(product);
     }
@@ -38,5 +50,13 @@ public class ProductController {
     @DeleteMapping("/{id}")
     public void deleteProduct(@PathVariable Long id) {
         service.deleteProduct(id);
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(
+            @PathVariable Long id,
+            @RequestBody Product updatedProduct) {
+        Product product = service.updateProduct(id, updatedProduct);
+        return ResponseEntity.ok(product);
     }
 }
